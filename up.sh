9289bd5f33a6e694110230940b4c33693dcd7266
nhan.dev <hey@nhan.dev>
2025-05-24 15:55:29 +0700

    rename

--- up.sh
+++ up.sh
@@ -1,4 +1,6 @@
 printf '{\n\t"at": "%s",\n\t"up": "%s"\n}' \
   "$(date -Iseconds)" \
   "$(uptime | awk -F'(  up |, )' '{ print ($3 ~ /user/) ? $2 : $2 " " $3 }')" \
-  > uptime.json
+  > up.json
+git add up.json
+git cmms up
