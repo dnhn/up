f33ec44182bf5f441c3f843ebb52b18a4196d175
nhan.dev <hey@nhan.dev>
2025-05-24 16:11:09 +0700

    format

--- up.sh
+++ up.sh
@@ -1,4 +1,4 @@
-printf '{\n\t"at": "%s",\n\t"up": "%s"\n}' \
+printf '{\n  "at": "%s",\n  "up": "%s"\n}' \
   "$(date -Iseconds)" \
   "$(uptime | awk -F'(  up |, )' '{ print ($3 ~ /user/) ? $2 : $2 " " $3 }')" \
   > up.json
